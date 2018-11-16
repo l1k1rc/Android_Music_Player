9b3952dadf35d35b73382d18d32ebf15ed12f904
l1k1rc <45100859+l1k1rc@users.noreply.github.com>
2018-11-16 15:28:46 +0100

    Add files via upload
    le background.xml et *.img dans /res/drawable/
    le activity_main.xml et song.xml dans /res/layout/
    le main.xml menu_drawer.xml et options.xml dans /res/menu/ ( a créer )

---
+++ MainActivity.java
@@ -0,0 +1,164 @@
+package com.linkin.musicprojectl3;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
+
+    private ArrayList<Song> songList;
+    private ListView songView;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        songView = (ListView) findViewById(R.id.song_list);
+        songList = new ArrayList<Song>();
+
+        // if the permission is obtained, the program can launch the activity
+        if (checkPermissionREAD_EXTERNAL_STORAGE(this)) {
+            Log.i("TAG", "HELLO THERE");
+            getSongList(); // own method to parse the storage
+
+            Collections.sort(songList, new Comparator<Song>() { // sort the song into the list
+                @Override
+                public int compare(Song o1, Song o2) {
+                    return o1.getTitle().compareTo(o2.getTitle());
+                }
+            });
+            SgAdapter songAdapter = new SgAdapter(this, songList);
+            songView.setAdapter(songAdapter); // displaying
+        }
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show();
+    }
+
+    /* Allow to check the permission to have an access in the internal storage of the user's phone */
+    public boolean checkPermissionREAD_EXTERNAL_STORAGE(
+            final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) context,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context, Manifest.permission.READ_EXTERNAL_STORAGE); // appel à showDialog
+
+                } else {
+                    ActivityCompat.requestPermissions((Activity) context,
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+    public void showDialog(final String msg, final Context context,
+                           final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[]{permission},
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+    /* Allow to parse the internal storage to get the music format */
+    public void getSongList() {
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+        if (musicCursor != null && musicCursor.moveToFirst()) {
+            //get columns
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            //add songs to list
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                songList.add(new Song(thisId, thisTitle, thisArtist));
+            }
+            while (musicCursor.moveToNext());
+        }
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.option:
+                return true;
+            case R.id.favoris:
+                return true;
+            case R.id.stats:
+                SgAdapter songAdapter = new SgAdapter(this, songList);
+                songView.setAdapter(songAdapter);
+                return true;
+            case R.id.quitter:
+                finish();
+                return true;
+        }
+        return false;
+    }
+
+}
---
+++ SgAdapter.java
@@ -0,0 +1,50 @@
+package com.linkin.musicprojectl3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class SgAdapter extends BaseAdapter {
+
+    private ArrayList<Song> songs;
+    private LayoutInflater layoutInflater;
+
+    public SgAdapter(Context c, ArrayList<Song> theSongs) {
+        this.songs = theSongs;
+        this.layoutInflater = LayoutInflater.from(c);
+    }
+
+    @Override
+    public int getCount() {
+        return songs.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LinearLayout songLay = (LinearLayout)layoutInflater.inflate
+                (R.layout.song, parent, false);
+        TextView songView = (TextView)songLay.findViewById(R.id.song_title);
+        TextView artistView = (TextView)songLay.findViewById(R.id.song_artist);
+        Song currSong = songs.get(position);
+        songView.setText(currSong.getTitle());
+        artistView.setText(currSong.getArtist());
+        songLay.setTag(position);
+        return songLay;
+    }
+}
